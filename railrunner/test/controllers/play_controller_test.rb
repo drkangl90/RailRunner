492edec5eb20ef5626874f5dd0c4f25c73b8c970
Dirk <hortensd@gmail.com>
2016-05-01 12:11:56 -0700

    Renaming 'Game' controller to 'Play' controller

--- railrunner/test/controllers/play_controller_test.rb
+++ railrunner/test/controllers/play_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class GameControllerTest < ActionController::TestCase
+class PlayControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
