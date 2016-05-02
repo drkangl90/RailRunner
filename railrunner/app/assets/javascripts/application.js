613c8c94433a7129f5cd2d8768920a6b0aea5477
Dirk <hortensd@gmail.com>
2016-05-01 17:43:26 -0700

    Adding WebGL assets to Rails application.

--- railrunner/app/assets/javascripts/application.js
+++ railrunner/app/assets/javascripts/application.js
@@ -10,7 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// Critical Libraries
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//
+// Third Party Dependencies
+//= require three
+//= require PointerLockControls
+//
+// Our Scripts (assets/*)
 //= require_tree .
